36fc28ae34875146bb045ffbec3426dd0443e78e
Filip Strajnar <filip.strajnar@gmail.com>
2023-11-28 12:46:23 +0100

    Generated necessary components and services.

--- arnes-frontend/src/app/app.module.ts
+++ arnes-frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewReservationComponent } from './components/new-reservation/new-reservation.component';
+import { AllReservationsComponent } from './components/all-reservations/all-reservations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewReservationComponent,
+    AllReservationsComponent
   ],
   imports: [
     BrowserModule,
