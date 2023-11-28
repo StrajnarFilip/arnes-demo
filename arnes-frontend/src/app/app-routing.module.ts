b47119cd9ea2c1e706d7d403a6c4afba42660356
Filip Strajnar <filip.strajnar@gmail.com>
2023-11-28 13:05:23 +0100

    Added some rudimentary routing.

--- arnes-frontend/src/app/app-routing.module.ts
+++ arnes-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllReservationsComponent } from './components/all-reservations/all-reservations.component';
+import { NewReservationComponent } from './components/new-reservation/new-reservation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reservations', component: AllReservationsComponent },
+  { path: 'new-reservation', component: NewReservationComponent },
+  { path: '**', redirectTo: 'reservations' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
