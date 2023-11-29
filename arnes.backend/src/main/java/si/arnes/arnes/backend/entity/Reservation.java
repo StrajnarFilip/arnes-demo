d64c6abf4b3164eebc06d34069be69b6602c7e28
Filip Strajnar <filip.strajnar@gmail.com>
2023-11-29 23:39:13 +0100

    Created Entity for reservations.

---
+++ arnes.backend/src/main/java/si/arnes/arnes/backend/entity/Reservation.java
@@ -0,0 +1,44 @@
+package si.arnes.arnes.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.OffsetDateTime;
+
+@Entity
+public class Reservation {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @JsonProperty("roomName")
+  private String room;
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
+  @JsonProperty("reserveFrom")
+  private OffsetDateTime reservedFrom;
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
+  @JsonProperty("reserveTo")
+  private OffsetDateTime reservedTo;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getRoom() {
+    return room;
+  }
+
+  public OffsetDateTime getReservedFrom() {
+    return reservedFrom;
+  }
+
+  public OffsetDateTime getReservedTo() {
+    return reservedTo;
+  }
+}
