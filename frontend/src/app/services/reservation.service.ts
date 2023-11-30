27a9c794394ec848837460c73bc1eb2fc01d4c00
Filip Strajnar <filip.strajnar@gmail.com>
2023-11-30 19:40:56 +0100

    Added explicit type annotations to methods in ReservationService.

--- frontend/src/app/services/reservation.service.ts
+++ frontend/src/app/services/reservation.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ReservationEntity } from '../entities/reservation-entity';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -17,13 +17,16 @@ export class ReservationService {
     );
   }
 
-  addReservation(reservation: Omit<ReservationEntity, 'id'>) {
+  addReservation(
+    reservation: Omit<ReservationEntity, 'id'>,
+  ): Observable<HttpResponse<string>> {
     return this.httpClient.post(`${this.BASE_URL}/reservation`, reservation, {
       observe: 'response',
+      responseType: 'text',
     });
   }
 
-  removeReservation(id: number) {
+  removeReservation(id: number): Observable<string> {
     return this.httpClient.delete(`${this.BASE_URL}/reservation/${id}`, {
       responseType: 'text',
     });
