434299f006e136168a16a1ea1ac9f691e45f00a8
Filip Strajnar <filip.strajnar@gmail.com>
2023-11-30 18:43:12 +0100

    Cleanup - removed unnecessary try-catch block.

--- backend/src/main/java/si/arnes/arnes/backend/Application.java
+++ backend/src/main/java/si/arnes/arnes/backend/Application.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
-    try {
-      SpringApplication.run(Application.class, args);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    SpringApplication.run(Application.class, args);
   }
 }
